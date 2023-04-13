11bed9b1f46da41ab1c63f7a4b6dbbaf07d8af45
nonahayrapetyan <73930671+nonahayrapetyan@users.noreply.github.com>
2023-04-13 19:47:27 +0400

    updated button and token

--- src/components/Button/button.styles.ts
+++ src/components/Button/button.styles.ts
@@ -1,4 +1,7 @@
 import Button from 'antd/es/button';
 import styled from 'styled-components';
+import token from '../../token/tokens.json';
 
-export const SButton = styled(Button)``;
+export const SButton = styled(Button)`
+  color: ${token.global.orange.value};
+`;
